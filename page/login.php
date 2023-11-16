bf1cd9f44134e5bcfa7cbb89c27289471d460eb2
NovaH06 <112965291+NovaH06@users.noreply.github.com>
2023-11-16 15:03:19 +0100

    yassquifautc

--- page/login.php
+++ page/login.php
@@ -1,25 +1,35 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <link rel="stylesheet" href="/style/login.css">
-  <title>Login Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/style/login.css">
+    <title>Login Page</title>
 </head>
+
 <body>
-  <div class="login-container">
     <h1>Login</h1>
-    <form id="login-form">
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
-      <label for="email">Mot de passe:</label>
-      <input type="password" id="email" name="email" required>
-      <button type="submit">Login</button>
-      <p>By clicking on <a href="#">"Sign up"</a>, you accept the</p> 
-        <p1 class="deco">Terms and Conditions of Use.</p1>
-    </form>
-  </div>
+    <div class=contain>
+
+        <form method="post" action='<?php echo $_SERVER["REQUEST_URI"]; ?>'>
+
+            <label for="email">Email :</label>
+            <input type="email" id="email" name="email" required class="input"><br><br>
+
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" required class="input"><br><br>
+            <div class="btn">
+                <input type="submit" value="Done" class="button-60">
+            </div>
+
+            <p class="ligne_inscription">By clicking on <a href="#">"Inscription"</a>, you accept the</p>
+            <p class="deco">Terms and Conditions of Use.</p>
+        </form>
+
+
+    </div>
 </body>
+
 </html>
