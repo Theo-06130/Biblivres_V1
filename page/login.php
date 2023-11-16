4e141928d3a5e289de761fa48eeaf9004a041650
NovaH06 <112965291+NovaH06@users.noreply.github.com>
2023-11-16 14:40:06 +0100

    yassquifautc

---
+++ page/login.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="/style/login.css">
+  <title>Login Page</title>
+</head>
+<body>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form id="login-form">
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      <label for="email">Mot de passe:</label>
+      <input type="password" id="email" name="email" required>
+      <button type="submit">Login</button>
+      <p>By clicking on <a href="#">"Sign up"</a>, you accept the</p> 
+        <p1 class="deco">Terms and Conditions of Use.</p1>
+    </form>
+  </div>
+</body>
+</html>
