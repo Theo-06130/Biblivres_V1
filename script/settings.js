2f24f778c003f92fb5e391ce125a0ba299ea85be
Sqwado <mateo.luque@ynov.com>
2023-11-23 17:44:19 +0100

    settings not finish

--- script/settings.js
+++ script/settings.js
@@ -1,11 +1,22 @@
 const Prenom = document.getElementById("Prenom");
-const Nom = document.getElementById("name");
+const Nom = document.getElementById("nom");
 const email = document.getElementById("email");
 const tel = document.getElementById("tel");
+
+const old_password = document.getElementById("old_password");
 const password = document.getElementById("password");
 const confirm_password = document.getElementById("confirm_password");
-const edit = document.getElementById("edit");
+
+const EditInfo = document.getElementById("EditInfo");
+const EditPass = document.getElementById("EditPass");
+
 const img_easter_egg = document.getElementById("img_easter_egg")
+
+const ChangeInformations = document.getElementById("ChangeInformations");
+const ChangePassword = document.getElementById("ChangePassword");
+
+const submitInfo = document.getElementById("submitInfo");
+const submitPass = document.getElementById("submitPass");
 
 let egg = 0
 function easter_egg() {
@@ -17,26 +28,72 @@ function easter_egg() {
     }
 }
 
+$('#submitInfo').click(function (e) {
+    if (Prenom.value == "" || Nom.value == "" || email.value == "" || tel.value == "") {
+        e.preventDefault();
+        alert("Veuillez remplir tous les champs !");
+    } else {
+        $('#ChangeInformations').submit();
+    }
+});
 
+$('#submitPass').click(function (e) {
+    if (password.value != confirm_password.value || password.value == "" || confirm_password.value == "" || old_password.value == "") {
+        e.preventDefault();
+        alert("Les mots de passe ne correspondent pas ou sont vides !");
+    } else {
+        $('#ChangePassword').submit();
+    }
 
-// function disable_input() {
-//     Prenom.disabled = true;
-//     Nom.disabled = true;
-//     email.disabled = true;
-//     tel.disabled = true;
-//     password.disabled = true;
-//     confirm_password.disabled = true;
-// }
+});
 
-// function enable_input() {
-//     Prenom.disabled = false;
-//     Nom.disabled = false;
-//     email.disabled = false;
-//     tel.disabled = false;
-//     password.disabled = false;
-//     confirm_password.disabled = false;
-//     console.log('dispo');
-// }
+EditInfo.addEventListener("click", function () {
+    if (Prenom.disabled) {
+        EditInfo.style.border = "2px solid #00f708";
+        Change_Info_State(false)
+        submitInfo.style.border = "2px solid #00f708";
+        submitInfo.disabled = false;
+    } else {
+        EditInfo.style.border = "2px solid #FF0000";
+        Change_Info_State(true)
+        submitInfo.style.border = "2px solid #FF0000";
+        submitInfo.disabled = true;
+    }
+});
 
+EditPass.addEventListener("click", function () {
+    if (password.disabled) {
+        EditPass.style.border = "2px solid #00f708";
+        Change_Pass_State(false)
+        submitPass.style.border = "2px solid #00f708";
+        submitPass.disabled = false;
+    } else {
+        EditPass.style.border = "2px solid #FF0000";
+        Change_Pass_State(true)
+        submitPass.style.border = "2px solid #FF0000";
+        submitPass.disabled = true;
+    }
+});
 
+window.onload = function () {
+    submitInfo.style.border = "2px solid #FF0000";
+    submitInfo.disabled = true;
+    submitPass.style.border = "2px solid #FF0000";
+    submitPass.disabled = true;
+    Change_Info_State(true)
+    Change_Pass_State(true)
+}
+
+function Change_Info_State(state) {
+    Prenom.disabled = state;
+    Nom.disabled = state;
+    email.disabled = state;
+    tel.disabled = state;
+}
+
+function Change_Pass_State(state) {
+    old_password.disabled = state;
+    password.disabled = state;
+    confirm_password.disabled = state;
+}
 
