ec2727262f50746a4fca473178c5cab77a7237c5
ibra-sko <ibrahim.sakotraore@gmail.com>
2023-11-23 17:40:35 +0100

    panier

--- page/panier.php
+++ page/panier.php
@@ -7,14 +7,15 @@
     <link rel="stylesheet" href="../style/panier.css">
 </head>
 <body class="panier">
-    <a href="#" class="link">Home</a>
+    <a href="home.php" class="link">Home</a>
     <section>
         <?php
+
         $database = new Database($_ENV["DB_HOST"], $_ENV["DB_PORT"], $_ENV["DB_DATABASE"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
         
 
         $conn = $database->getConnection();
-        $conn->query("");
+        
 
         if (isset($_SESSION["Id_client"]) && !empty($_SESSION["Id_client"])) {
             $idClient = $_SESSION["Id_client"];
@@ -41,16 +42,18 @@
             }
         }
 
+
         // Récupérer les articles du panier
-        $sql = "SELECT * FROM Articles_panier";
+        $sql = "SELECT * FROM Article_panier";
         $result = $conn->query($sql);
+
 
         if ($result->rowCount() > 0) {
             echo "<table border='1'>
             <tr>
-            <th>ID_article_panier</th>
-            <th>ID_client</th>
-            <th>ID_livre</th>
+            <th>Id_article_panier</th>
+            <th>Id_client</th>
+            <th>Id_livre</th>
             <th>Quantité</th>
             </tr>";
 
@@ -58,14 +61,15 @@
 
             while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr>
-                <td>".$row["ID_article_panier"]."</td>
-                <td>".$row["ID_client"]."</td>
-                <td>".$row["ID_livre"]."</td>
-                <td>".$row["quantity"]."</td>
+                <td>".(isset($row["ID_article_panier"]) ? $row["ID_article_panier"] : "")."</td>
+                <td>".(isset($row["ID_client"]) ? $row["ID_client"] : "")."</td>
+                <td>".(isset($row["ID_livre"]) ? $row["ID_livre"] : "")."</td>
+                <td>".(isset($row["quantity"]) ? $row["quantity"] : "")."</td>
                 </tr>";
+            
 
                 // Calculez le total
-                $sql = "SELECT * FROM Livres WHERE ID_livre = ".$row["ID_livre"];
+                $sql = "SELECT * FROM Livres WHERE ID_livre = ".(isset($row["ID_livre"]) ? $row["ID_livre"] : "");
                 $result2 = $conn->query($sql);
                 if ($result2->rowCount() > 0) {
                     while ($row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
@@ -86,7 +90,7 @@
         ?>
 
         <!-- Formulaire pour ajouter un article -->
-        <form method="post" action="">
+        <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
             <label for="id_client">ID Client:</label>
             <input type="text" name="id_client" required>
 
@@ -101,7 +105,6 @@
     </section>
 </body>
 </html>
-
 
 
 
