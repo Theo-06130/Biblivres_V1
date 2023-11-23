2d5fba70f436baf2c3c51cfc15362c61a8db5112
ibra-sko <ibrahim.sakotraore@gmail.com>
2023-11-23 17:01:41 +0100

    panier

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ if ($parts[1] == "home") {
     include("page/types.php");
 } elseif ($parts[1] == "parametre") {
     include("page/parametre.php");
+} elseif ($parts[1] == "panier") {
+    include("page/panier.php");
 } elseif ($parts[1] == "langues") {
     include("page/langues.php");
 } else {
